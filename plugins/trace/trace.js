78f54e34c502ba8282c041c9819a2fac5552a5f6
Tim van der Staaij <tvdstaaij@users.noreply.github.com>
2015-07-18 01:34:28 +0200

    simplify trace plugin

--- plugins/trace/trace.js
+++ plugins/trace/trace.js
@@ -2,18 +2,9 @@ module.exports = function loadPlugin(resources) {
 
     var log = resources.log;
     
-    var asyncDummy = function(cb) {
-        process.nextTick(function() {
-            cb(null, true);
-        });
-    };
-    
     return {
-        enable: asyncDummy,
-        disable: asyncDummy,
         handleMessage: function(message, meta) {
-            log.trace('[trace.handleMessage] meta:', meta,
-                      ', message:', message);
+            log.info('handleMessage: meta =', meta, ', message =', message);
         }
     };
 };
