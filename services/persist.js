545aeaf078ab8ffebe56bf6a503d9ac87a413fb9
Tim van der Staaij <tvdstaaij@users.noreply.github.com>
2015-07-20 02:06:38 +0200

    migrate persist service to bluebird

--- services/persist.js
+++ services/persist.js
@@ -1,14 +1,14 @@
 var path = require('path');
+var Promise = require('bluebird');
 var O = require('observed');
 var store = require('store');
-var Q = require('q');
 
 var appRoot, log, storage;
 
 module.exports.init = function(resources) {
     log = resources.log;
     appRoot = resources.app.root;
-    storage = store(path.join(appRoot, 'persist'));
+    storage = Promise.promisifyAll(store(path.join(appRoot, 'persist')));
 };
 
 module.exports.provides = function(context) {
@@ -16,42 +16,28 @@ module.exports.provides = function(context) {
     var storageId = context.name + '.' + context.type;
 
     methods.load = function(cb) {
-        return loadStorage().then(
-            function(container) {
+        return storage.loadAsync(storageId).then(function(container) {
                 return container;
-            },
-            function() {
+            }).catch(function() {
                 return createStorage();
-            }
-        ).then(
-            function(container) {
+            }).then(function(container) {
                 O(container.data).on('change', function() {
-                    saveStorage(container).catch(function(error) {
+                    storage.addAsync(container).catch(function(error) {
                         log.error('Failed saving persistent storage ' +
                                   container.id + ':', error);
-                    }).done();
+                    });
                 });
                 return container.data;
             }
         ).nodeify(cb);
     };
 
-    function loadStorage() {
-        return Q.nfcall(storage.load, storageId);
-    }
-
     function createStorage() {
         var newStorage = {
             id: storageId,
             data: {}
         };
-        return Q.nfcall(storage.add, newStorage).then(function() {
-            return newStorage;
-        });
-    }
-
-    function saveStorage(container) {
-        return Q.nfcall(storage.add, container);
+        return storage.addAsync(newStorage).return(newStorage);
     }
 
     return methods;
