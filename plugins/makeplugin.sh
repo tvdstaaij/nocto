f867bdf6008141c3d8de40a2b6b0fa42c347cf96
Tim van der Staaij <tvdstaaij@users.noreply.github.com>
2015-07-25 17:29:51 +0200

    fix bug introduced with earlier plugin skeleton change

--- plugins/makeplugin.sh
+++ plugins/makeplugin.sh
@@ -7,6 +7,6 @@ fi
 skel=".skeleton"
 cd "${0%/*}" # Make this directory (plugins) the working directory
 mkdir "$1"
-sed "s/$skel/$1/g" $skel/package.json >  "$1/package.json"
+sed "s/skeleton/$1/g" $skel/package.json >  "$1/package.json"
 cat $skel/script.js > "$1/$1.js"
 cat $skel/config.json > "$1/config.json"
