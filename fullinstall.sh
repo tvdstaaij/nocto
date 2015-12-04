be6cdf1ac0340ec15228ac1d3fe1eea4ea3cf664
Tim van der Staaij <tvdstaaij@users.noreply.github.com>
2015-12-04 22:09:21 +0100

    Add production flag to npm install

--- fullinstall.sh
+++ fullinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-npm install
+npm install --production
 for plugindir in plugins/*
 do
-    ( cd "$plugindir" && [ -f package.json ] && npm install )
+    ( cd "$plugindir" && [ -f package.json ] && npm install --production )
 done
